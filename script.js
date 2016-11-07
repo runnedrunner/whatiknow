8551f4d902419b4c0071e9ea8bcfd1e4b7d631e1
runnedrunner <auszhou@nuevaschool.org>
2016-11-07 09:07:34 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 function logArgs(args) {
   console.log(args)
 }
-function coolfunction(
+function coolfunction(args) {
+  return 0;
+}
 var functionString = logArgs.toString();
 console.log(functionString)
 var consoleLogExample = $("#console-log-example")
